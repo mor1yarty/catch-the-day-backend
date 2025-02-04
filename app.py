ecf708271f03f25abd2dd457d0a1b1d6019a867c
Yuma Moriya <y.moriya.113@gmail.com>
2025-02-04 22:38:06 +0900

    任意の日付を選べるように仕様変更

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# データのスキーマを定義するためのクラス
+# JSONのスキーマ。'area'と'date'のフィールドを定義
 class EchoMessage(BaseModel):
-    message: str | None = None
+    area: str | None = None
+    date: str | None = None
 
+# 許容するエリアの値
 area_dict = {
     "白馬":{"group":"北部", "locacion_id":48141, "lat":36.7, "lon":137.85},
     "志賀高原":{"group":"北部", "locacion_id":48066, "lat":36.7, "lon":138.4},
@@ -28,6 +30,23 @@ area_dict = {
     "斑尾・妙高":{"group":"北部", "locacion_id":48061, "lat":36.9, "lon":138.2},
     "菅平":{"group":"中部", "locacion_id":48216, "lat":36.3, "lon":138.4},
     "木曽":{"group":"南部", "locacion_id":48531, "lat":35.9, "lon":137.7},
+}
+
+# 許容する日付の値
+dates_dict = {
+    "今日":0,
+    "明日":1,
+    "明後日":2,
+    "3日後":3,
+    "4日後":4,
+    "5日後":5
+}
+
+# 天候データのキャッシュデータ取得
+weatherinfo_cache = {
+    "cache_date": None,
+    "weatherinfo": {},
+    "snow_amount": {}
 }
 
 # 'src'ディレクトリ内のファイルを '/static' パスで公開
@@ -46,22 +65,39 @@ def hello_world():
 
 @app.post("/api/echo")
 def echo(message: EchoMessage):
-    # messageフィールドが空の場合のチェック
-    if not message.message:
-        raise HTTPException(status_code=400, detail="Invalid JSON: 'message'フィールドに値を指定してください。")
+    # areaフィールドの存在チェック
+    if not message.area:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid JSON: 'area'フィールドに値を指定してください。"
+        )
+    # dateフィールドの存在チェック
+    if not message.date:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid JSON: 'date'フィールドに値を指定してください。"
+        )
 
     # 前後の空白を除去
-    echo_message = message.message.strip()
+    area_value = message.area.strip()
+    date_value = message.date.strip()
 
-    # 入力がarea_dictに存在するかチェック
-    if echo_message not in area_dict:
+    # 入力されたエリアが許容リストに存在するかチェック
+    if area_value not in area_dict:
         allowed = "、".join(area_dict.keys())
         raise HTTPException(
             status_code=400,
             detail=f"無効なエリアです。以下のいずれかを選んでください: {allowed}"
         )
+    # 入力された日付が許容リストに存在するかチェック
+    if date_value not in dates_dict:
+        allowed_dates = "、".join(dates_dict.keys())
+        raise HTTPException(
+            status_code=400,
+            detail=f"無効な日付です。以下のいずれかを選んでください: {allowed_dates}"
+        )
 
     # 有効なエリアの場合、天気情報と雪量情報を取得する
-    weather_forcast = get_weather_info(area_dict[echo_message]["lat"], area_dict[echo_message]["lon"])
-    snow_amount = get_local_amount(area_dict[echo_message]["locacion_id"])
+    weather_forcast = get_weather_info(area_dict[message.area]["lat"], area_dict[message.area]["lon"], dates_dict[message.date])
+    snow_amount = get_local_amount(area_dict[message.area]["locacion_id"])
     return {"snow_amount": snow_amount, "weather_forcast": weather_forcast}
--- getWeatherInfo/owm_api_weatherforcast.py
+++ getWeatherInfo/owm_api_weatherforcast.py
@@ -7,9 +7,6 @@ if os.environ.get("APP_ENV", "development") == "development":
     load_dotenv()
 OWM_API_KEY = os.environ.get("OWM_API_KEY")
 
-# 明日の日付をMMDD形式で取得
-tomorrow = datetime.now() + timedelta(days=1)
-date_str = tomorrow.strftime("%Y-%m-%d")
 
 weather_jpn = {
     "Snow": "雪",
@@ -27,7 +24,10 @@ weather_jpn = {
     "Squall": "スコール",
     "Tornado": "竜巻"
 }
-def get_weather_info(lat, lon):
+def get_weather_info(lat, lon, days):
+    # 明日の日付をMMDD形式で取得
+    tomorrow = datetime.now() + timedelta(days)
+    date_str = tomorrow.strftime("%Y-%m-%d")
     # 指定した緯度経度の直近の気象予報データの取得
     jma_url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&APPID={OWM_API_KEY}"
     response = requests.get(jma_url)
@@ -40,12 +40,12 @@ def get_weather_info(lat, lon):
             weather_tomorrow_json[f'{weather["dt_txt"].split(" ")[1][:5]}'] = {
                 "weather": weather_jpn[weather["weather"][0]["main"]],
                 "temp": f'{round(weather["main"]["temp"] - 273.15, 1)}℃',
-                "snow": weather["snow"]["3h"] if "snow" in weather else 0,
+                "snow": f'{weather["snow"]["3h"]}mm' if "snow" in weather else "0mm",
                 "icon": weather["weather"][0]["icon"]
             }
     del weather_tomorrow_json["00:00"], weather_tomorrow_json["03:00"]
     return weather_tomorrow_json
 
 if __name__ == "__main__":
-    weahter_info = get_weather_info(36, 138)
+    weahter_info = get_weather_info(36, 138 ,1)
     print(weahter_info)
